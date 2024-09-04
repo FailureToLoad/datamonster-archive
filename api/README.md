dfc11fb0fe9f2d883bc59c08e77de2f61fedb94b
Dave M. <davejmason@outlook.com>
2024-09-04 15:07:05 -0700

    Local updates (#31)
    * reverts web to always run on 8090
    
    * adds compose for local development
    
    * updates docs

--- api/README.md
+++ api/README.md
@@ -6,6 +6,5 @@ An API written in Go 1.22.6 for managing Kingdom Death: Monster campaigns.
 
 Ensure you have at least Go 1.22.6 installed.  
 Install cmake, then check out the make file for build and run commands.  
-Check out the .env.example file for the environment variables you'll need to set.  
 
-When running the api it'll bind to port 8090.
+When running the api it'll bind to port 8080.
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,27 +1,19 @@
-import {defineConfig, loadEnv} from 'vite';
+import {defineConfig} from 'vite';
 import path from 'path';
 import react from '@vitejs/plugin-react-swc';
 
-export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, process.cwd());
-  const PORT = `${env.VITE_PORT ?? '8090'}`;
-  return {
-    plugins: [react()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-        '@types': path.resolve(__dirname, './src/__generated__/graphql'),
-      },
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@types': path.resolve(__dirname, './src/__generated__/graphql'),
     },
-    server: {
-      host: '0.0.0.0',
-      strictPort: true,
-      port: Number(PORT),
-    },
-    preview: {
-      host: '0.0.0.0',
-      strictPort: true,
-      port: 8090,
-    },
-  };
+  },
+  server: {
+    host: true,
+    strictPort: true,
+    port: 8090,
+  },
 });
